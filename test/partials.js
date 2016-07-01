0ad72ec5e07765aad62dbac3f41728a443d32be3
Magnus Hoff <maghoff@gmail.com>
2016-07-01 15:18:35 +0200

    Add smoke tests for partials resolvers

--- test/partials.js
+++ test/partials.js
@@ -5,9 +5,9 @@ const path = require('path');
 const chai = require('chai');
 const assert = chai.assert;
 
-function testRender(template, data, expected) {
+function testRenderCore(resolverClass, template, data, expected) {
 	return done => {
-		const partials = new mustachio.partials.Fs(path.join(__dirname, 'partials'));
+		const partials = new resolverClass(path.join(__dirname, 'partials'));
 		mustachio.string(template).render(data, partials).string().then(actual => {
 			assert.equal(expected, actual);
 			done();
@@ -15,6 +15,20 @@ function testRender(template, data, expected) {
 	};
 }
 
+describe('FsNoCache', function() {
+	const testRender = testRenderCore.bind(null, mustachio.partials.FsNoCache);
+
+	it('should resolve partials file', testRender("{{>apekatt}}", {}, "apekatt\n"));
+});
+
 describe('Fs', function() {
+	const testRender = testRenderCore.bind(null, mustachio.partials.Fs);
+
+	it('should resolve partials file', testRender("{{>apekatt}}", {}, "apekatt\n"));
+});
+
+describe('FsWatch', function() {
+	const testRender = testRenderCore.bind(null, mustachio.partials.FsWatch);
+
 	it('should resolve partials file', testRender("{{>apekatt}}", {}, "apekatt\n"));
 });
