4bbfe776d9047cfefb92f582d6a00e8ec90341a9
Magnus Hoff <maghoff@gmail.com>
2016-10-01 00:45:13 +0200

    Refactor to share Template class. This fixes #14

--- index.js
+++ index.js
@@ -7,35 +7,36 @@ const Context = require('./lib/context');
 const compiler = require('./lib/compiler');
 
 
-function Rendering(stream) {
-	this._stream = stream;
-}
-
-Rendering.prototype.string = function () {
-	return new Promise((resolve, reject) => {
-		const buf = [];
-		this._stream.on('data', chunk => buf.push(chunk));
-		this._stream.on('error', reject);
-		this._stream.on('end', () => resolve(buf.join('')));
-	});
-};
-
-Rendering.prototype.stream = function () {
-	return this._stream;
-};
-
-
 function Template(template) {
 	this.template = template;
 }
 
 Template.prototype.render = function (data, partials) {
-	const r = this.template(Context.createRoot(data, partials));
-	const reader = new GeneratorStream(r);
+	const context = Context.createRoot(data, partials);
 
-	data.flush = () => reader.flush();
+	const iterator =
+		(this.template instanceof Promise)
+			? (function* () {
+				const t2 = yield Promise.resolve(this.template);
+				yield* t2(context);
+			}.bind(this))()
+			: this.template(context);
 
-	return new Rendering(reader);
+	const stream = new GeneratorStream(iterator);
+
+	data.flush = () => stream.flush();
+
+	return {
+		string: () => {
+			return new Promise((resolve, reject) => {
+				const buf = [];
+				stream.on('data', chunk => buf.push(chunk));
+				stream.on('error', reject);
+				stream.on('end', () => resolve(buf.join('')));
+			});
+		},
+		stream: () => stream
+	};
 };
 
 
@@ -45,23 +46,9 @@ function resolver(opts) {
 	const partialsResolver = opts.partialsResolver || new partials.FsWatch(opts.root, opts.suffixes);
 
 	return (templateName, data) => {
-		const context = Context.createRoot(data || {}, partialsResolver);
-
 		const template = partialsResolver.get(templateName);
 
-		const iterator =
-			(template instanceof Promise)
-				? (function* () {
-					const t2 = yield Promise.resolve(template);
-					yield* t2(context);
-				})()
-				: template(context);
-
-		const stream = new GeneratorStream(iterator);
-
-		data.flush = () => stream.flush();
-
-		return new Rendering(stream);
+		return new Template(template).render(data || {}, partialsResolver);
 	};
 }
 
